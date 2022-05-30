4069ddfb3804b8c6cbbf896b1d2ba36e2caa2e77
simransdsu <simran5590@gmail.com>
2022-05-30 13:56:49 -0700

    Initial Commit

---
+++ We Split/We_SplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  We_SplitApp.swift
+//  We Split
+//
+//  Created by Simran Preet Singh Narang on 2022-05-30.
+//
+
+import SwiftUI
+
+@main
+struct We_SplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
